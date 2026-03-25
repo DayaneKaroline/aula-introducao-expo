392c56af4effd21a1715999de6e0fbf9e20701cb
DayaneKaroline <dayanefalves33@gmail.com>
2026-03-24 22:47:07 -0300

    at
    Exemplo- 03

--- App.js
+++ App.js
@@ -2,14 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
 import Exemplo01 from './src/exemplos/ex-01';
-import Atividade01 from './src/atividades/atv-01';
 import Exemplo02 from './src/exemplos/ex-02';
+import Exemplo03 from './src/exemplos/ex-03';
+
+import Atividade01 from './src/atividades/atv-01';
 import Atividade02 from './src/atividades/atv-02';
+//import Atividade03 from './src/atividades/atv-03';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade02 />
+      <Exemplo03 />
       <StatusBar style="light"/>
     </View>
   );
