d65f71c78cc40e783bb87da9895f18f1e4e1f818
DayaneKaroline <dayanefalves33@gmail.com>
2026-05-05 20:36:53 -0300

    atv
    Atividade 06

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import Exemplo02 from './src/exemplos/ex-02';
 import Exemplo03 from './src/exemplos/ex-03';
 import Exemplo04 from './src/exemplos/ex-04';
 import Exemplo05 from './src/exemplos/ex05';
+import Exemplo06 from './src/exemplos/ex06';
+import Exemplo07 from './src/exemplos/ex07';
 
 
 import Atividade01 from './src/atividades/atv-01';
@@ -13,13 +15,14 @@ import Atividade02 from './src/atividades/atv-02';
 import Atividade03 from './src/atividades/atv-03';
 import Atividade04 from './src/atividades/atv-04';
 import Atividade05 from './src/atividades/atv-05';
+import Atividade06 from './src/atividades/atv-06';
 
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade05 />
+      <Atividade06 />
       <StatusBar style="light"/>
     </View>
   );
